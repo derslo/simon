a8c1755d2b6e064251faa8cfb8877efb929694ce
Stephan Losse <stephan.losse@me.com>
2017-05-28 00:40:48 +0200

    optimized crud controller

--- app/Http/Controllers/Admin/StorageCrudController.php
+++ app/Http/Controllers/Admin/StorageCrudController.php
@@ -10,12 +10,80 @@ use App\Http\Requests\StorageCrudRequest as UpdateRequest;
 
 class StorageCrudController extends CrudController
 {
-    public function setup() {
+    public function setup()
+    {
         $this->crud->setModel("App\Models\Storage");
         $this->crud->setRoute("admin/storage");
         $this->crud->setEntityNameStrings('Speichersystem', 'Speichersysteme');
-
         $this->crud->setFromDb();
+
+        $this->crud->setColumns(
+            [
+                [
+                    'name'  => 'form_name',
+                    'label' => 'Name',
+                ],
+            ]
+        );
+
+        /**
+         * Insert a Numberfield Field for Amount
+         */
+        $this->crud->addField(
+            [
+                'name'  => 'amount',
+                'label' => "Menge",
+                'type'  => 'number',
+                // 'suffix' => ".00",
+            ]
+        );
+
+        /**
+         * Insert an Radio-Field for Unit
+         */
+        $this->crud->addField(
+            [
+                'name'    => 'unit', // the name of the db column
+                'label'   => 'Einheit', // the input label
+                'type'    => 'radio',
+                'options' => [ // the key will be stored in the db, the value will be shown as label;
+                    'PB' => "PB",
+                    'TB' => "TB",
+                    'GB' => "GB",
+                ],
+                // optional
+                'inline'  => true, // show the radios all on the same line?
+            ]
+        );
+
+        /**
+         * Insert an Radio-Field for Type
+         */
+        $this->crud->addField(
+            [
+                'name'    => 'type', // the name of the db column
+                'label'   => 'Typ', // the input label
+                'type'    => 'radio',
+                'options' => [ // the key will be stored in the db, the value will be shown as label;
+                    'TB' => "TB",
+                    'GB' => "GB",
+                ],
+                // optional
+                'inline'  => true, // show the radios all on the same line?
+            ]
+        );
+
+        /**
+         * Insert a Checkbox Field for Raid
+         */
+        $this->crud->addField(
+            [
+                'name'  => 'raid',
+                'label' => "Raid",
+                'type'  => 'checkbox',
+            ]
+        );
+
     }
 
     public function store(StoreRequest $request)
